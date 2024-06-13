a78e6d2e1ba3f3071b60ab1830a57dd15936f77d
carisaelam <carisaelam@gmail.com>
2024-06-13 14:38:56 -0400

    Finish project

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
+require 'dotenv/load'
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
 file_path = '/Users/carisaelam/odin-repos/ruby/odin-event-manager/event_attendees.csv'
+registration_times = []
+registration_days = []
+api_key = ENV['API_KEY']
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -12,34 +18,55 @@ def clean_phone(phone)
   if phone.length == 10
     phone
   elsif phone.length > 10 && phone[0] == '1'
-    phone[1..-1]
+    phone[1..]
   else
-    phone = '0000000000'
+    '0000000000'
   end
 end
 
-# def legislators_by_zipcode(zip)
-#   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-#   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+def find_most_common_reg_time(registration_times)
+  reg_time_objects = registration_times.map { |time| Time.parse(time) }
+  hour_groups = reg_time_objects.group_by(&:hour)
+  counts = hour_groups.transform_values(&:count)
+  most_common_hour = counts.max_by { |_, count| count }.first
+  puts "The most common hour for registrations was #{most_common_hour}. This means that most people were registering between 1:00 pm and 2:00 pm"
+end
 
-#   begin
-#     civic_info.representative_info_by_address(
-#       address: zip,
-#       levels: 'country',
-#       roles: %w[legislatorUpperBody legislatorLowerBody]
-#     ).officials
-#   rescue StandardError
-#     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-#   end
-# end
+def find_most_common_reg_day(registration_days)
+  parsed_dates = registration_days.map { |date| Date.strptime(date, '%m/%d/%y') }
 
-# def save_thank_you_letter(id, form_letter)
-#   Dir.mkdir('output') unless Dir.exist?('output')
-#   filename = "output/thanks_#{id}.html"
-#   File.open(filename, 'w') do |file|
-#     file.puts form_letter
-#   end
-# end
+  weekdays = parsed_dates.map(&:wday)
+
+  most_common_weekday = weekdays.max_by { |day| weekdays.count(day) }
+
+  days_of_week = %w[Sunday Monday Tuesady Wednesday Thursday Friday Saturday]
+
+  most_common_day = days_of_week[most_common_weekday]
+  puts "The most common day for voter registration was #{most_common_day}."
+end
+
+def legislators_by_zipcode(zip, api_key)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = api_key
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 
 puts 'EventManager initialized.'
 
@@ -57,7 +84,17 @@ contents.each do |row|
   name = row[:first_name]
   phone = clean_phone(row[:homephone].gsub(/[^\d]/, ''))
   zipcode = clean_zipcode(row[:zipcode])
-  # legislators = legislators_by_zipcode(zipcode)
-  # form_letter = erb_template.result(binding)
-  # save_thank_you_letter(id, form_letter)
+  reg_date = row[:regdate].split(' ')
+  reg_time = reg_date[1]
+  reg_days = reg_date[0]
+  registration_times.push(reg_time)
+  registration_days.push(reg_days)
+
+  legislators = legislators_by_zipcode(zipcode, api_key)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 end
+
+find_most_common_reg_time(registration_times)
+
+find_most_common_reg_day(registration_days)
